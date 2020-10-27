ba4e89a1067872204cf0c8bb5a62f06f34b4ad03
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-10-27 19:40:32 -0400

    Built header and container components to render

---
+++ src/Components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Header() {
+    return(
+        <div className="jumbotron text-center">
+            <h1>Employee Directory</h1>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/pages/Employees.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Container from '../Components/Container';
+import Header from '../Components/Header';
+
+
+function Employees() {
+    return (
+        <div>
+            <Header />
+            <Container>
+                
+            </Container>
+        </div>
+    )
+}
+
+export default Employees;
